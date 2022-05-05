3711041a01447fb8e3abacbdc6d7178d7568328d
Deekshant Yadav <d33kshant@gmail.com>
2022-05-05 22:33:03 +0530

    Update routes

--- routes/group.route.js
+++ routes/group.route.js
@@ -1,3 +1,9 @@
 const router = require('express').Router()
+const { createGroup, getGroupInfo, updateGroup } = require('../controllers/group.controller')
+const { authenticate, authorize } = require('../controllers/user.controller')
+
+router.post('/create', authenticate, createGroup)
+router.post('/update/:name', authenticate, updateGroup)
+router.get('/:name', authorize, getGroupInfo)
 
 module.exports = router
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const { createPost, updatePost, getFeedPosts, deletePost, getUserPosts, likePost } = require('../controllers/post.controller')
+const { createPost, updatePost, getFeedPosts, deletePost, getUserPosts, likePost, getLikes } = require('../controllers/post.controller')
 const { authenticate, authorize } = require('../controllers/user.controller')
 
 router.post('/create', authenticate, createPost)
 router.post('/update/:id', authenticate, updatePost)
 router.post('/delete/:id', authenticate, deletePost)
 router.post('/like/:id', authenticate, likePost)
+router.get('/like/:id', authorize, getLikes)
 router.get('/user/:id', authorize, getUserPosts)
-router.get('/', authorize, getFeedPosts)
 
 module.exports = router
