e0718fd39b518972b04c93bdaee97077c9f5e507
Deekshant Yadav <d33kshant@gmail.com>
2022-05-05 16:13:01 +0530

    Update routes

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
-const { createPost, updatePost, getFeedPosts } = require('../controllers/post.controller')
+const { createPost, updatePost, getFeedPosts, deletePost, getUserPosts, likePost } = require('../controllers/post.controller')
 const { authenticate, authorize } = require('../controllers/user.controller')
 
 router.post('/create', authenticate, createPost)
 router.post('/update/:id', authenticate, updatePost)
+router.post('/delete/:id', authenticate, deletePost)
+router.post('/like/:id', authenticate, likePost)
+router.get('/user/:id', authorize, getUserPosts)
 router.get('/', authorize, getFeedPosts)
 
 module.exports = router
