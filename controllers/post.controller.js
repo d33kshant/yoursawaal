4c8e8cae9c3ad41e921fcfbc55146e13aa600b8b
Deekshant Yadav <d33kshant@gmail.com>
2022-05-05 16:12:29 +0530

    Updated controllers

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,22 +1,177 @@
-const createPost = (req, res) => {
-	// TODO: Create post from req.body, Required authentication
+const Post = require('../models/post.model')
+
+const createPost = async (req, res) => {
+	const body = req.body.body
+	const ref = req.body.ref || null
+	const external_link = req.body.external_link
+	const is_sponsored = false
+	const likes = []
+	const user = req.user
+
+	if (!user) {
+		return res.json({
+			error: "Authantication failed"
+		})
+	}
+
+	try {
+		const post = new Post({
+			body,
+			ref,
+			external_link,
+			is_sponsored,
+			likes,
+			author: user.uid
+		})
+		await post.save()
+		res.json({
+			message: "Post created successfully",
+			payload: post
+		})
+	} catch (error) {
+		res.json({
+			error: "Something went wrong.",
+			payload: error
+		})
+	}
 }
 
-const likePost = (req, res) => {
-	// TODO: Like post, Required authentication and post id
+const likePost = async (req, res) => {
+	const user_id = req.user.uid
+	const { id: post_id } = req.params
+
+	if (!post_id) {
+		return res.json({
+			error: "Missing post id in params"
+		})
+	}
+
+	try {
+		const post = await Post.findById(post_id)
+		if (post) {
+			if (post.likes.indexOf(user_id) !== -1) {
+				post.likes = post.likes.filter(user => user !== user_id)
+				await post.save()
+				res.json({
+					message: 'Post disliked.',
+					liked: false
+				})
+			} else {
+				post.likes.push(user_id)
+				await post.save()
+				res.json({
+					message: "Post liked.",
+					liked: true
+				})
+			}
+		} else {
+			return res.json({
+				error: "Post not found.",
+			})
+		}
+	} catch (error) {
+		res.json({
+			error: "Something went wrong.",
+			payload: error
+		})
+	}
+}
+
+const getUserPosts = async (req, res) => {
+	const { id: user_id } = req.params
+	const { page } = req.query || 1
+	const POST_PER_PAGE = 10
+
+	const offset = ((Math.max(page, 1) - 1) * POST_PER_PAGE)
+
+	try {
+		const total = await Post.find({ author: user_id }).count()
+		const posts = await Post.find({ author: user_id }).skip(offset).limit(POST_PER_PAGE)
+		res.json({
+			total_page: Math.ceil(total / POST_PER_PAGE),
+			posts
+		})
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
 }
 
 const getFeedPosts = (req, res) => {
 	// TODO: Get posts related to current user if exist, Required authorization if user exist
 }
 
-const updatePost = (req, res) => {
-	// TODO: Update post from req.boyd, Required authentication and post id
+const updatePost = async (req, res) => {
+	const user = req.user
+	const { id: post_id } = req.params
+	const {
+		body,
+		external_link,
+	} = req.body
+
+	if (!post_id) {
+		return res.json({
+			error: "Post id missing in params"
+		})
+	}
+
+	try {
+		const post = await Post.findById(post_id)
+		if (post.author === user.uid) {
+
+			const filter = { _id: post_id }
+			const update = {
+				body,
+				external_link,
+				last_update: Date.now()
+			}
+			await Post.updateOne(filter, update)
+			res.json({
+				message: "Post has been updated.",
+			})
+		} else {
+			res.json({
+				error: "Operation not permited."
+			})
+		}
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
+}
+
+const deletePost = async (req, res) => {
+	const { id: post_id } = req.params
+	const { uid: user_id } = req.user
+
+	try {
+		const post = await Post.findById(post_id)
+		if (post) {
+			if (post.author === user_id) {
+				await Post.deleteOne({ _id: post_id })
+				res.json({
+					message: "Post deleted successfully"
+				})
+			} else {
+				res.json({
+					error: "Operation not permited."
+				})
+			}
+		}
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
 }
 
 module.exports = {
 	createPost,
 	updatePost,
 	likePost,
+	deletePost,
+	getUserPosts,
 	getFeedPosts
 }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,18 +1,110 @@
-const signupUser = (req, res) => {
-	// TODO: Regiater user
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user.model')
+
+const signupUser = async (req, res) => {
+	const { email, password, full_name } = req.body
+	
+	if (!email || !password || !full_name) {
+		return res.json({
+			error: "Required fields can not be empty."
+		})
+	}
+
+	try {
+		const _user = await User.findOne({ email: email })
+		if (_user) {
+			return res.json({
+				error: "Email is already registred. Try another one.",
+			})
+		}
+
+		const hash = await bcrypt.hash(password, 10)
+		const user = new User({
+			email,
+			name: full_name,
+			password: hash
+		})
+		await user.save()
+		res.json({
+			message: "Signup successfully."
+		})
+	} catch (error) {
+		res.json({
+			error: "Something went wrong.",
+			payload: error
+		})
+	}
 }
 
-const loginUser = (req, res) => {
-	// TODO: Login user
+const loginUser = async (req, res) => {
+	const { email, password } = req.body
+	
+	if (!email || !password) {
+		return res.json({
+			error: "Required fields can not be empty."
+		})
+	}
+
+	try {
+		const user = await User.findOne({ email })
+		if (user) {
+			if (await bcrypt.compare(password, user.password)) {
+				const token = jwt.sign({
+					email: user.email,
+					uid: user._id
+				}, process.env.SECRET)
+				res.cookie('token', token, { httpOnly: true });
+				res.json({
+					message: "Logged in successfully.",
+					token
+				})
+			} else {
+				res.json({
+					error: "Invalid username or password." // Password didn't match
+				})	
+			}
+		} else {
+			res.json({
+				error: "Invalid username or password." // Email didn't match
+			})
+		}
+	} catch (error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
 }
 
 const authenticate = (req, res, next) => {
-	// TODO: Authenticate jwt token, Required login if not
-	next()
+	const token = req.cookies.token
+	if (token) {
+		const payload = jwt.verify(token, process.env.SECRET)
+		if (payload) {
+			req.user = payload
+			next()
+		} else {
+			res.json({
+				error: "Authantication failed",
+				login: false
+			})
+		}
+	} else {
+		res.json({
+			error: "Authantication failed",
+			login: false
+		})
+	}
 }
 
 const authorize = (req, res, next) => {
-	// TODO: Authorize jwt token 
+	const token = req.cookies.token
+	if (token) {
+		const payload = jwt.verify(token, process.env.SECRET)
+		if (payload) {
+			req.user = payload
+		}
+	}
 	next()
 }
 
