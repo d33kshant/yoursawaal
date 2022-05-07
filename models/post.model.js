327d450c490a98aa2bc61d9b19991c8bc669ed14
Deekshant Yadav <d33kshant@gmail.com>
2022-05-07 10:01:03 +0530

    Update models

--- models/post.model.js
+++ models/post.model.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema({
-	author: String,
-	body: String,
-	ref: String,
-	group: String,
-	is_sponsored: Boolean,
-	external_link: String,
-	likes: [String],
-	last_update: {
+	author: String,			// Author id
+	body: String,			// Content of post
+	ref: String,			// Id of reference post if any
+	group: String,			// Group in which posted
+	is_sponsored: Boolean,	// Post is sponsored or not, For admin use
+	external_link: String,	// External link
+	likes: [String],		// List of user that liked this post
+	last_update: {			// Date post last updated
 		type: Date,
 		default: Date.now,
 	},
-	created_on: {
+	created_on: {			// Date post created
 		type: Date,
 		default: Date.now
 	},
--- models/user.model.js
+++ models/user.model.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
-	name: String,
-	about: String,
-	occupation: String,
-	country: String,
-	gander: String,
-	birthday: Date,
-	password: String,
-	hobbies: String,
-	interests: String,
-	experience: [String],
-	education: [String],
-	avatar: {
+	name: String,				// User's name
+	about: String,				// User's bio
+	occupation: String,			// User's occupation
+	country: String,			// User's country
+	gander: String,				// User's gender
+	birthday: Date,				// User's Birthday
+	password: String,			// User's password, Hashed
+	hobbies: String,			// User's hobbies, comma seprated
+	interests: String,			// User's interest, comma sepreted
+	experience: [String],		// List of User's experience
+	education: [String],		// List of User's educations
+	avatar: {					// Link to User's avatar
 		type: String,
 		default: 'avatar.png'
 	},
-	banner: {
+	banner: {					// Link to User's banner image
 		type: String,
 		default: 'banner.png'
 	},
-	email: {
+	email: {					// User's email
 		type: String,
 		unique: true
 	},
-	joined: {
+	joined: {					// Date user joined
 		type: Date,
 		default: Date.now
 	}
