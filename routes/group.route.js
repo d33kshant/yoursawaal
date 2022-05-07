e02ce03dff09fc75b5649011536176cacff78e63
Deekshant Yadav <d33kshant@gmail.com>
2022-05-07 15:22:23 +0530

    Add new routes

--- routes/group.route.js
+++ routes/group.route.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
-const { createGroup, getGroupInfo, updateGroup } = require('../controllers/group.controller')
+const { createGroup, getGroupInfo, updateGroup, joinGroup, getGroups } = require('../controllers/group.controller')
 const { authenticate, authorize } = require('../controllers/user.controller')
 
 router.post('/create', authenticate, createGroup)
 router.post('/update/:name', authenticate, updateGroup)
+router.post('/join/:name', authenticate, joinGroup)
+router.get('/random', authorize, getGroups)
 router.get('/:name', authorize, getGroupInfo)
 
 module.exports = router
