a9705647475230c373348eaa141334ed102999e6
Deekshant Yadav <d33kshant@gmail.com>
2022-05-10 18:57:00 +0530

    Add passport and facebook auth mods

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cookieparser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
 const mongoose = require('mongoose')
+const facebookStrategy = require('passport-facebook').Strategy
+const User = require('./models/user.model')
 
 // Routes
 const userRoute = require('./routes/user.route')
@@ -14,6 +18,50 @@ const DB_URI = process.env.DB_URI
 const app = express()
 app.use(express.json())
 app.use(cookieparser())
+app.use(session({
+	secret: process.env.SECRET
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new facebookStrategy(
+	{
+		clientID: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		callbackURL: '/api/user/callback/facebook',
+		profileFields: [ 'id', 'displayName', 'gender', 'email', 'picture.type(large)' ]
+	},
+	async (token, refreshToken, profile, done) => {
+		try {
+			console.log(profile)
+			const email = profile.emails[0].value
+			const user = await User.findOne({ email })
+			if (user) {
+				done(null, user)
+			} else {
+				const newUser = new User({
+					name: profile.displayName,
+					email: email,
+					gender: profile.gender,
+					birthday: profile.birthday,
+					avatar: profile.photos[0].value
+				})
+				await newUser.save()
+				done(null, user)
+			}
+		} catch(error) {
+			done(error)
+		}
+	}
+))
+
+passport.serializeUser((user, done) => {
+	done()
+})
+
+passport.deserializeUser((user, done) => {
+	done(null, user.id)
+})
 
 app.use('/api/user', userRoute)
 app.use('/api/group', groupRoute)
