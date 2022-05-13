d18c08eacb392c6a7d2227c1ba5ec27bbf4d20ea
Deekshant Yadav <d33kshant@gmail.com>
2022-05-13 22:12:59 +0530

    Added functions to get users and posts

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+
 const User = require('../models/user.model')
+const Post = require('../models/post.model')
 
 const loginAdmin = async (req, res) => {
 	const { email, password } = req.body
@@ -47,13 +49,68 @@ const loginAdmin = async (req, res) => {
 	}
 }
 
-const isAdmin = (req, res, next) => {
+const getUsers = async (req, res) => {
+	const { page } = req.query
+	const USER_PER_PAGE = 10
+	const offset = ((Math.max(page, 1) - 1) * USER_PER_PAGE)
+
+	try {
+		const totalUsers = await User.find({}).count()
+		const users = await User.find({}).skip(offset).limit(USER_PER_PAGE)
+		for (const user of users) {
+			delete user._doc.password
+		}
+		res.json({
+			total_page: Math.ceil(totalUsers/USER_PER_PAGE),
+			users
+		})
+	} catch(error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
+}
+
+const getPosts = async (req, res) => {
+	const page = Number(req.query.page) || 1
+	const POST_PER_PAGE = 10
+	const offset = ((Math.max(page, 1) - 1) * POST_PER_PAGE)
+
+	try {
+		const totalPosts = await Post.find({}).count()
+		const posts = await Post.find({}).skip(offset).limit(POST_PER_PAGE)
+		res.json({
+			total_page: Math.ceil(totalPosts/POST_PER_PAGE),
+			posts
+		})
+	} catch(error) {
+		res.json({
+			error: "Something went wrong."
+		})
+	}
+}
+
+const isAdmin = async (req, res, next) => {
 	const uid = req.user.uid
-	const user = await User.findById(uid)
-	(user?.is_admin) ? next() : res.json({ error: "Operation not permitted." })
+	User.findById(uid, (error, doc) => {
+		if (error) {
+			return res.json({
+				error: "Semething went wrong"
+			})
+		}
+		if (doc.is_admin) {
+			next()
+		} else {
+			res.json({
+				error: "Operation not permited."
+			})
+		}
+	})
 }
 
 module.exports = {
 	isAdmin,
 	loginAdmin,
+	getUsers,
+	getPosts,
 }
