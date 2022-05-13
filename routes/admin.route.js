e1aa3986fa98a494ea6376b6f299ed31dc724aec
Deekshant Yadav <d33kshant@gmail.com>
2022-05-13 22:13:16 +0530

    Added routes to get users and posts

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
-const { loginAdmin } = require('../controllers/admin.controller')
+const { authenticate } = require('../controllers/user.controller')
+const { loginAdmin, getUsers, isAdmin, getPosts } = require('../controllers/admin.controller')
 
 router.post('/login', loginAdmin)
+
+router.get('/get/users', authenticate, isAdmin, getUsers)
+router.get('/get/posts', authenticate, isAdmin, getPosts)
 
 module.exports = router
