53aa2ac499eeaf5b33fe860fd1dd4cee59c95262
Deekshant Yadav <d33kshant@gmail.com>
2022-05-14 11:09:46 +0530

    Added route to get groups

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const { authenticate } = require('../controllers/user.controller')
-const { loginAdmin, getUsers, isAdmin, getPosts } = require('../controllers/admin.controller')
+const { loginAdmin, getUsers, isAdmin, getPosts, getGroups } = require('../controllers/admin.controller')
 
 router.post('/login', loginAdmin)
 
 router.get('/get/users', authenticate, isAdmin, getUsers)
 router.get('/get/posts', authenticate, isAdmin, getPosts)
+router.get('/get/groups', authenticate, isAdmin, getGroups)
 
 module.exports = router
