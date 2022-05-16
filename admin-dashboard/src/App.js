a168cdfd38f05e7c06997045a7abef01ca6049ed
Deekshant Yadav <d33kshant@gmail.com>
2022-05-16 18:24:09 +0530

    Added routing

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,8 +1,49 @@
+import { useState } from 'react'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate
+} from 'react-router-dom'
+import AuthProvider from './contexts/AuthContext'
+import GroupsPage from './pages/GroupsPage'
+import HomaPage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import PostsPage from './pages/PostsPage'
+import UsersPage from './pages/UsersPage'
 import './styles/App.css'
 
 function App() {
+
+	const [user, setUser] = useState(null)
+
+	const login = _user => setUser(_user)
+	const logout = () => setUser(null)
+
+	if (!user) {
+		return (
+			<AuthProvider>
+				<Router>
+					<Routes>
+						<Route path='/' element={<LoginPage />} />
+						<Route path='*' element={<Navigate to='/' />} />
+					</Routes>
+				</Router>
+			</AuthProvider>
+		)
+	}
+
 	return (
-		<div className='app'>App</div>
+		<AuthProvider value={{ user, login, logout }} >
+			<Router>
+				<Routes>
+					<Route path='/' element={ <HomaPage/> } />
+					<Route path='/posts' element={ <PostsPage /> } />
+					<Route path='/groups' element={ <GroupsPage /> } />
+					<Route path='/users' element={ <UsersPage /> } />
+				</Routes>
+			</Router>
+		</AuthProvider>
 	)
 }
 
