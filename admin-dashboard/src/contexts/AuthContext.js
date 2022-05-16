1cb605805ae2db8c71c15c0d1d397cea35fd6fbe
Deekshant Yadav <d33kshant@gmail.com>
2022-05-16 18:23:10 +0530

    Create auth context

---
+++ admin-dashboard/src/contexts/AuthContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react'
+export const AuthContext = createContext({ user: null, login: (user)=>{}, logout:()=>{} })
+const AuthProvider = AuthContext.Provider
+export default AuthProvider
