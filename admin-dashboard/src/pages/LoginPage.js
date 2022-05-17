56df5e021c4328bd4733d15a8e38ece3e4183833
Deekshant Yadav <d33kshant@gmail.com>
2022-05-17 16:06:29 +0530

    Add login feature

--- admin-dashboard/src/pages/LoginPage.js
+++ admin-dashboard/src/pages/LoginPage.js
@@ -1,7 +1,41 @@
+import { useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+
 function LoginPage() {
+
+	const [state, setState] = useState({ email: '', password: '' })
+	const { login } = useContext(AuthContext)
+
+	const onFormSubmit = async event => {
+		event.preventDefault()
+		const response = await fetch('/api/admin/login', {
+			method: "post",
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(state)
+		})
+		const data = await response.json()
+		if (data.error) {
+			return alert(data.error)
+		}
+		login({ token: data.token })
+	}
+
+	const onInputChange = (key, value) => {
+		const newState = { ...state }
+		newState[key] = value
+		setState(newState)
+	}
+
 	return (
 		<div className="login-page-container">
-			LoginPage
+			<form onSubmit={onFormSubmit}>
+				<input type="email" name='email' value={state.email} onChange={event=>onInputChange('email', event.target.value)} />
+				<input type="password" name='password' value={state.password} onChange={event=>onInputChange('password', event.target.value)} />
+				<button type='submit'>Login</button>
+			</form>
 		</div>
 	)
 }
