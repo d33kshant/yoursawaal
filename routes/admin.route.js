89f48f68c2be65ef913e9824f93e6f64a79b0a80
Deekshant Yadav <d33kshant@gmail.com>
2022-05-19 10:37:20 +0530

    Add new routes to get item by id

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 const { authenticate } = require('../controllers/user.controller')
-const { loginAdmin, getUsers, isAdmin, getPosts, getGroups } = require('../controllers/admin.controller')
+const { loginAdmin, getUsers, isAdmin, getPosts, getGroups, getUserById, getPostById, getGroupById } = require('../controllers/admin.controller')
 
 router.post('/login', loginAdmin)
 
 router.get('/get/users', authenticate, isAdmin, getUsers)
 router.get('/get/posts', authenticate, isAdmin, getPosts)
 router.get('/get/groups', authenticate, isAdmin, getGroups)
+
+router.get('/get/users/:id', authenticate, isAdmin, getUserById)
+router.get('/get/posts/:id', authenticate, isAdmin, getPostById)
+router.get('/get/groups/:id', authenticate, isAdmin, getGroupById)
 
 module.exports = router
