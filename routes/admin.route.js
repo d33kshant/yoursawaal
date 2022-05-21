8cc6e7187c15023b188f9a391e4efe51b202bc94
Deekshant Yadav <d33kshant@gmail.com>
2022-05-21 16:09:44 +0530

    Add route to delete items

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -12,7 +12,10 @@ const {
 	getStats,
 	updateUser, 
 	updatePost,
-	updateGroup
+	updateGroup,
+	deleteUser,
+	deletePost,
+	deleteGroup
 } = require('../controllers/admin.controller')
 
 router.post('/login', loginAdmin)
@@ -30,5 +33,9 @@ router.get('/get/groups/:id', authenticate, isAdmin, getGroupById)
 router.post('/set/users/:id', authenticate, isAdmin, updateUser)
 router.post('/set/posts/:id', authenticate, isAdmin, updatePost)
 router.post('/set/groups/:id', authenticate, isAdmin, updateGroup)
+
+router.post('/delete/users/:id', authenticate, isAdmin, deleteUser)
+router.post('/delete/posts/:id', authenticate, isAdmin, deletePost)
+router.post('/delete/groups/:id', authenticate, isAdmin, deleteGroup)
 
 module.exports = router
