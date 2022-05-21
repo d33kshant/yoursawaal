d7d59276f117413703e8bba9f94c774bf034f4e5
Deekshant Yadav <d33kshant@gmail.com>
2022-05-21 09:36:41 +0530

    Added stats and cards

--- admin-dashboard/src/pages/HomePage.js
+++ admin-dashboard/src/pages/HomePage.js
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 
 function HomaPage() {
+
+	const [stats, setStats] = useState({})
+
+	useEffect(() => {
+		const fetchStats = async () => {
+			const response = await fetch('/api/admin/get/stats')
+			const data = await response.json()
+			if (data.error) return alert(data.error)
+			setStats(data)
+		}
+		fetchStats()
+	}, [])
+
 	return (
 		<>
 			<NavBar />
 			<div className='home-page-container'>
-				<Link to="/users">Users</Link>
-				<Link to="/posts">Posts</Link>
-				<Link to="/groups">Groups</Link>
+				<Link className='data-card' to="/users">
+					<h3>Users</h3>
+					<p>Total Items: {stats.total_users || 'Loading'}</p>
+				</Link>
+				<Link className='data-card' to="/posts">
+					<h3>Posts</h3>
+					<p>Total Items: {stats.total_posts || 'Loading'}</p>
+				</Link>
+				<Link className='data-card' to="/groups">
+					<h3>Groups</h3>
+					<p>Total Items: {stats.total_groups || 'Loading'}</p>
+				</Link>
 			</div>
 		</>
 	)
