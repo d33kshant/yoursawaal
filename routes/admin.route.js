a724c891fb701f5e1164e73fdcfcaf43eac605ad
Deekshant Yadav <d33kshant@gmail.com>
2022-05-30 18:13:37 +0530

    Update route methods

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { authenticate } = require('../controllers/user.controller')
-const { 
+const {
 	loginAdmin,
 	getUsers,
 	isAdmin,
@@ -10,7 +10,7 @@ const {
 	getPostById,
 	getGroupById,
 	getStats,
-	updateUser, 
+	updateUser,
 	updatePost,
 	updateGroup,
 	deleteUser,
@@ -22,20 +22,20 @@ router.post('/login', loginAdmin)
 
 router.get('/get/stats', authenticate, isAdmin, getStats)
 
-router.get('/get/users', authenticate, isAdmin, getUsers)
-router.get('/get/posts', authenticate, isAdmin, getPosts)
-router.get('/get/groups', authenticate, isAdmin, getGroups)
+router.get('/users', authenticate, isAdmin, getUsers)
+router.get('/posts', authenticate, isAdmin, getPosts)
+router.get('/groups', authenticate, isAdmin, getGroups)
 
-router.get('/get/users/:id', authenticate, isAdmin, getUserById)
-router.get('/get/posts/:id', authenticate, isAdmin, getPostById)
-router.get('/get/groups/:id', authenticate, isAdmin, getGroupById)
+router.get('/users/:id', authenticate, isAdmin, getUserById)
+router.get('/posts/:id', authenticate, isAdmin, getPostById)
+router.get('/groups/:id', authenticate, isAdmin, getGroupById)
 
-router.post('/set/users/:id', authenticate, isAdmin, updateUser)
-router.post('/set/posts/:id', authenticate, isAdmin, updatePost)
-router.post('/set/groups/:id', authenticate, isAdmin, updateGroup)
+router.put('/users/:id', authenticate, isAdmin, updateUser)
+router.put('/posts/:id', authenticate, isAdmin, updatePost)
+router.put('/groups/:id', authenticate, isAdmin, updateGroup)
 
-router.post('/delete/users/:id', authenticate, isAdmin, deleteUser)
-router.post('/delete/posts/:id', authenticate, isAdmin, deletePost)
-router.post('/delete/groups/:id', authenticate, isAdmin, deleteGroup)
+router.delete('/users/:id', authenticate, isAdmin, deleteUser)
+router.delete('/posts/:id', authenticate, isAdmin, deletePost)
+router.delete('/groups/:id', authenticate, isAdmin, deleteGroup)
 
 module.exports = router
